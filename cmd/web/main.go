3a071af9ebfc31a35ed2d3ec18dc2752f9ec953a
Jonathan <j-clemons@tuta.io>
2023-09-18 22:23:01 -0500

    create routes.go

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -30,26 +30,6 @@ func main() {
         logger: logger,
     }
 
-    // Use the http.NewServeMux() function to initialize a new servemux, then
-    // register the home function as the handler for the "/" URL pattern.
-    mux := http.NewServeMux()
-
-    // create a file server which serves files out of the "./ui/static"
-    // dir. Note that the path given to the http.Dir func is relative
-    // to the project directory root.
-    fileServer := http.FileServer(http.Dir("./ui/static/"))
-
-    // use the mux.Handle() func to register the file server as the handler
-    // for all URL paths that start with "/static/".
-    // For matching paths, we strip the "/static" prefix before the request
-    // reaches the file server.
-    mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-
-    // Register the other application routes as normal.
-    mux.HandleFunc("/", app.home)
-    mux.HandleFunc("/snippet/view", app.snippetView)
-    mux.HandleFunc("/snippet/create", app.snippetCreate)
-
     // Print a log message to say that the server is starting.
     logger.Info("starting server", "addr", *addr)
 
@@ -58,7 +38,7 @@ func main() {
     // and the servemux we just created. If http.ListenAndServe() returns an 
     // error we use the log.Fatal() to log the error and exit.
     // Note any error returned by http.ListenAndServe() is always non-nil.
-    err := http.ListenAndServe(*addr, mux)
+    err := http.ListenAndServe(*addr, app.routes())
 
     logger.Error(err.Error())
     os.Exit(1)
---
+++ cmd/web/routes.go
@@ -0,0 +1,28 @@
+package main
+
+import "net/http"
+
+// the routes() method returns a servemux containing app routes
+func (app *application) routes() *http.ServeMux {
+    // Use the http.NewServeMux() function to initialize a new servemux, then
+    // register the home function as the handler for the "/" URL pattern.
+    mux := http.NewServeMux()
+
+    // create a file server which serves files out of the "./ui/static"
+    // dir. Note that the path given to the http.Dir func is relative
+    // to the project directory root.
+    fileServer := http.FileServer(http.Dir("./ui/static/"))
+
+    // use the mux.Handle() func to register the file server as the handler
+    // for all URL paths that start with "/static/".
+    // For matching paths, we strip the "/static" prefix before the request
+    // reaches the file server.
+    mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+    // Register the other application routes as normal.
+    mux.HandleFunc("/", app.home)
+    mux.HandleFunc("/snippet/view", app.snippetView)
+    mux.HandleFunc("/snippet/create", app.snippetCreate)
+
+    return mux
+}
