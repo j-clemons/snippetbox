c00b8cfcaf2e0e901454956bc5f9fd1ae17fe4d8
Jonathan <j-clemons@tuta.io>
2023-09-25 22:45:11 -0500

    update routing to use httprouter

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -7,19 +7,15 @@ import (
     "strconv"
 
     "github.com/j-clemons/snippetbox/internal/models"
+
+    "github.com/julienschmidt/httprouter"
 )
 
 // Define a home handler function which write a byte slice containing
 // "Hello from Snippetbox" as the response body.
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
-    // check if request URL path matches "/".
-    // If not then use notFound() helper to send a 404
-    // must return from the handler or else the handler will continue 
-    // executing all code below
-    if r.URL.Path != "/" {
-        app.notFound(w)
-        return
-    }
+    // can remove check for r.URL.Path != "/" because httprouter 
+    // matches path exactly
 
     snippets, err := app.snippets.Latest()
     if err != nil {
@@ -38,10 +34,12 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 
 // Add a snippetView handler function
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
+    params := httprouter.ParamsFromContext(r.Context())
+
     // extract the value of the id parameter from query string
     // and attempt to convert to int. If not int or less than 1
     // return 404 error
-    id, err := strconv.Atoi(r.URL.Query().Get("id"))
+    id, err := strconv.Atoi(params.ByName("id"))
     if err != nil || id < 1 {
         app.notFound(w)
         return
@@ -65,20 +63,14 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
     app.render(w, r, http.StatusOK, "view.tmpl", data)
 }
 
-// Add a snippetCreate handler function
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
-    // use r.Method to check whether the request is using POST or not.
-    if r.Method != http.MethodPost {
-        // if it's not, use the w.WriteHeader() method to send a 405 status code
-        // and the w.Write() method to write a "Method Not Allowed"
-        // response body. We then return from the function so that the 
-        // subsequent code is not executed.
-        w.Header().Set("Allow", http.MethodPost)
+    w.Write([]byte("display the form for creating a new snippet..."))
+}
 
-        // use the clientError() helper
-        app.clientError(w, http.StatusMethodNotAllowed)
-        return
-    }
+// Add a snippetCreate handler function
+func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
+    // no longer need to check if request is POST because this is 
+    // done automatically by httprouter
 
     // creating dummy data
     title := "0 snail"
@@ -93,6 +85,5 @@ func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
     }
 
     // redirect the user to the relevant page for the snippet
-    http.Redirect(w, r, fmt.Sprintf("/snippet/view?id=%d", id), http.StatusSeeOther)
-    // w.Write([]byte("Create a new snippet..."))
+    http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther)
 }
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -3,33 +3,33 @@ package main
 import (
     "net/http"
 
+    "github.com/julienschmidt/httprouter"
     "github.com/justinas/alice"
 )
 
 // the routes() method returns a http.Handler containing app routes
 func (app *application) routes() http.Handler {
-    // Use the http.NewServeMux() function to initialize a new servemux, then
-    // register the home function as the handler for the "/" URL pattern.
-    mux := http.NewServeMux()
+    // intialize the router
+    router := httprouter.New()
+
+    router.NotFound = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+        app.notFound(w)
+    })
 
     // create a file server which serves files out of the "./ui/static"
     // dir. Note that the path given to the http.Dir func is relative
     // to the project directory root.
     fileServer := http.FileServer(http.Dir("./ui/static/"))
-
-    // use the mux.Handle() func to register the file server as the handler
-    // for all URL paths that start with "/static/".
-    // For matching paths, we strip the "/static" prefix before the request
-    // reaches the file server.
-    mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+    router.Handler(http.MethodGet, "/static/*filepath", http.StripPrefix("/static", fileServer))
 
     // Register the other application routes as normal.
-    mux.HandleFunc("/", app.home)
-    mux.HandleFunc("/snippet/view", app.snippetView)
-    mux.HandleFunc("/snippet/create", app.snippetCreate)
+    router.HandlerFunc(http.MethodGet, "/", app.home)
+    router.HandlerFunc(http.MethodGet, "/snippet/view/:id", app.snippetView)
+    router.HandlerFunc(http.MethodGet, "/snippet/create", app.snippetCreate)
+    router.HandlerFunc(http.MethodPost, "/snippet/create", app.snippetCreatePost)
 
     // create a middleware chain using alice 
     standard := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
 
-    return standard.Then(mux)
+    return standard.Then(router)
 }
