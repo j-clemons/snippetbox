5f940de6a8214f509c3466ac95308a778b33c735
Jonathan <j-clemons@tuta.io>
2023-10-20 22:21:45 -0500

    add unit testing

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -24,7 +24,11 @@ type templateData struct {
 
 // create a function that returns a formatted time.Time object
 func humanDate(t time.Time) string {
-    return t.Format("02 Jan 2006 at 15:04")
+    if t.IsZero() {
+        return ""
+    }
+
+    return t.UTC().Format("02 Jan 2006 at 15:04")
 }
 
 var functions = template.FuncMap{
