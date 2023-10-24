737b4d441e1e80adce606be3872d379787f142c0
Jonathan <j-clemons@tuta.io>
2023-10-23 22:00:28 -0500

    add mocks

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -21,8 +21,8 @@ import (
 // define an application struct to hold the app-wide dependencies
 type application struct {
     logger         *slog.Logger
-    snippets       *models.SnippetModel
-    users          *models.UserModel
+    snippets       models.SnippetModelInterface
+    users          models.UserModelInterface
     templateCache  map[string]*template.Template
     formDecoder    *form.Decoder
     sessionManager *scs.SessionManager
